4319826ae4d3653ea0a98ee8adc3cf89d63eb4a3
Tetiana Furmanets <tetyana.best.89@ukr.net>
2026-03-08 07:59:58 +0100

    hw9

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,9 @@
 // lib/api/api.ts
-
 import axios from 'axios';
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + '/api', 
-  withCredentials: true,
+  withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
   },
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
-//next.config.ts
-
+// next.config.ts
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-
+  reactStrictMode: true, 
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'ac.goit.global',
+        hostname: 'ac.goit.global', 
       },
     ],
   },
