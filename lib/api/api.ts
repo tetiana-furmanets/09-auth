89d4c006f5f7a96850c0e375a38ec5acc34c0e04
Tetiana Furmanets <tetyana.best.89@ukr.net>
2026-03-08 10:59:45 +0100

    hw9

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,8 @@
 // lib/api/api.ts
 import axios from 'axios';
 
+console.log('NEXT_PUBLIC_API_URL =', process.env.NEXT_PUBLIC_API_URL);
+
 export const nextServer = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + '/api', 
   withCredentials: true, 
